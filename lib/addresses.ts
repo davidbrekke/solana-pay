08722e2940ac1b40cd6350691333d05c3402b8e1
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-04-06 16:51:03 -0500

    finished tutorial

---
+++ lib/addresses.ts
@@ -0,0 +1,12 @@
+import { PublicKey } from '@solana/web3.js'
+
+// Your shop wallet address
+export const shopAddress = new PublicKey(
+  '8mVjocvKtZBKS9j8765s9tSAW8cH9vtHkTp1my4ZGtiv'
+)
+export const usdcAddress = new PublicKey(
+  'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+)
+export const couponAddress = new PublicKey(
+  'CpVSSMYxMFoyrhfTtKSZu767xrr6ytfqkdp13n4DXQmS'
+)
